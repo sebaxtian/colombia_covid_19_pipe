c0bdd36972e5ea45fb3363d948f7420b1a2e08a8
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2020-04-08 12:01:11 -0500

    Update run script

--- run.sh
+++ run.sh
@@ -5,9 +5,14 @@
 #
 
 echo "Running Colombia Covid 19 Pipeline ..."
+# Load Python environment
 source .venv/bin/activate
+# Go to source code
 cd ./src
+# Run Python script
 python colombia_covid_19_pipe.py
+# Go back to main directory
 cd ../
+# Finish
 echo "Finished !"
 echo "The Pipeline output is generated within ./output directory."
